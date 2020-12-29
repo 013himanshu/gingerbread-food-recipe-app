356a74b6a205d872c0e8543f565c397620ffd7b3
Himanshu Kumawat <013himanshu@gmail.com>
2020-12-30 02:58:10 +0530

    added splashscreen made with MotionLayout

---
+++ app/src/main/java/com/example/gingerbread/SplashscreenActivity.kt
@@ -0,0 +1,38 @@
+package com.example.gingerbread
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.databinding.DataBindingUtil
+import com.example.gingerbread.databinding.ActivitySplashscreenBinding
+
+class SplashscreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashscreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_splashscreen)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splashscreen)
+
+        val motion = binding.constraintLayout
+        motion.addTransitionListener(object: MotionLayout.TransitionListener{
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startActivity(Intent(this@SplashscreenActivity, MainActivity::class.java))
+                finish()
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+
+            }
+
+        })
+    }
+}
