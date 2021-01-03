e0982d0940452bb58548b8a4f83ec925d4d8d833
Himanshu Kumawat <013himanshu@gmail.com>
2021-01-03 19:29:15 +0530

    added recipe row layout

--- app/src/main/java/com/example/gingerbread/Fragments/RecipeFragment.kt
+++ app/src/main/java/com/example/gingerbread/Fragments/RecipeFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.gingerbread.R
+import com.example.gingerbread.databinding.FragmentRecipeBinding
+import org.jsoup.helper.DataUtil
 
 class RecipeFragment : Fragment() {
-
+    private lateinit var binding: FragmentRecipeBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe, container, false)
+        //return inflater.inflate(R.layout.fragment_recipe, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_recipe, container, false)
+
+        binding.shimmerRecyclerView.showShimmer()
+
+        return binding.root
     }
 
 }
