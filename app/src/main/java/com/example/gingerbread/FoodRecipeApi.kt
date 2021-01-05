57b6a61d5aa3f744656a1959aacc5d33a927ff85
Himanshu Kumawat <013himanshu@gmail.com>
2021-01-05 13:45:00 +0530

    created recipe api interface

---
+++ app/src/main/java/com/example/gingerbread/FoodRecipeApi.kt
@@ -0,0 +1,15 @@
+package com.example.gingerbread
+
+import com.example.gingerbread.model.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipeApi {
+
+    @GET("/recipes/complexSearch")
+    suspend fun getRecipes(
+            @QueryMap queries: Map<String, String>
+    ): Response<FoodRecipe>
+
+}
